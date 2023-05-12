9bbd5b5bb904dff4d59f66a599cbd757a5badb59
kalexgama <kassio.alex.g@gmail.com>
2023-05-12 19:55:11 -0400

    rota inicial confirgurada

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(routes:{
+      '/home':(context) => const Home();
+    })
+  }
+}
