1a35bec2231c52813c4ad9dd4bd3f6cb5b2aa37d
kalexgama <kassio.alex.g@gmail.com>
2023-05-12 20:27:00 -0400

    tela de login modificada

--- lib/src/login.dart
+++ lib/src/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sisteminha/src/cadastro.dart';
 
 void main() {
   runApp(Login());
@@ -8,37 +9,51 @@ class Login extends StatelessWidget {
   const Login({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Nome de usuário',
-              ),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark),
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text('Login'),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Nome de usuário',
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação do botão de login
+                  },
+                  child: Text('Login'),
+                ),
+                SizedBox(height: 24.0),
+                Text("ou"),
+                SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                        context, '/cadastro'); // Ação do botão de login
+                  },
+                  child: Text('Cadastro'),
+                ),
+              ],
             ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Senha',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () {
-                // Ação do botão de login
-              },
-              child: Text('Login'),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
