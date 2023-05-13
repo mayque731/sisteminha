e903a8f9ebea70cc41f71cac6e13d29a128c1812
Mayque731 <mayque.731@gmail.com>
2023-05-12 20:33:52 -0400

    tela de cadastro alterada

--- lib/src/cadastro.dart
+++ lib/src/cadastro.dart
@@ -24,6 +24,40 @@ class _Cadastro extends State<Cadastro> {
               ),
             ),
           ),
+          body: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação do botão de login
+                  },
+                  child: Text('Login'),
+                ),
+              ],
+            ),
+          ),
         ));
   }
 }
