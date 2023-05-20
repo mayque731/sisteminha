c5ae95d0ac662ca824d6e69538cb76e0f25d8f85
Mayque731 <mayque.731@gmail.com>
2023-05-19 20:21:05 -0400

    concertado

--- lib/src/login.dart
+++ lib/src/login.dart
@@ -10,36 +10,64 @@ class Login extends StatefulWidget {
 }
 
 class _Login extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  var userController = TextEditingController();
+  var senhaController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark),
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text('Login'),
-          ),
-          body: Padding(
-            padding: EdgeInsets.all(30),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text('Login'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(30),
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
+                  controller: userController,
                   decoration: InputDecoration(
                     labelText: 'Nome de usuário',
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe um nome';
+                    } else if (value == 'kassio' || value == 'mayque') {
+                      return value;
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(height: 16.0),
                 TextFormField(
+                  controller: senhaController,
                   decoration: InputDecoration(
                     labelText: 'Senha',
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe uma senha';
+                    } else if (value == 'senha123') {
+                      return value;
+                    }
+                    return null;
+                  },
                   obscureText: true,
                 ),
                 SizedBox(height: 24.0),
                 ElevatedButton(
                   onPressed: () {
                     // Ação do botão de login
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushNamed(context, '/cadastro');
+                    }
                   },
                   child: Text('Logar'),
                 ),
@@ -56,6 +84,8 @@ class _Login extends State<Login> {
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
