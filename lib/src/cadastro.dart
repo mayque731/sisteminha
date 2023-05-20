4bc868accf173867a13c24144d6ff9c512e0923a
kalexgama <kassio.alex.g@gmail.com>
2023-05-19 20:28:08 -0400

    cadastro fungando

--- lib/src/cadastro.dart
+++ lib/src/cadastro.dart
@@ -5,70 +5,85 @@ void main() {
 }
 
 class Cadastro extends StatefulWidget {
-  _Cadastro createState() => _Cadastro();
+  _CadastroState createState() => _CadastroState();
 }
 
-class _Cadastro extends State<Cadastro> {
-  Widget build(BuildContext context) {
-    //TextEditingController _controller = new TextEditingController();
-    var nomeController = TextEditingController();
-    var senhaController = TextEditingController();
-    var emailController = TextEditingController();
-    var tamanhosenha = senhaController.text.length;
+class _CadastroState extends State<Cadastro> {
+  final _formKey = GlobalKey<FormState>();
+  var nomeController = TextEditingController();
+  var senhaController = TextEditingController();
+  var emailController = TextEditingController();
 
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark),
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: const Text(
-              "Cadastro",
-              style: TextStyle(
-                fontFamily: "roboto",
-                fontSize: 30,
-              ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            "Cadastro",
+            style: TextStyle(
+              fontFamily: "roboto",
+              fontSize: 30,
             ),
           ),
-          body: Padding(
-            padding: EdgeInsets.all(16.0),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
+                  controller: nomeController,
                   decoration: InputDecoration(
                     labelText: 'Nome',
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe o nome';
+                    }
+                    return null;
+                  },
                 ),
-                SizedBox(height: 16.0),
+                SizedBox(height: 8.0),
                 TextFormField(
+                  controller: emailController,
                   decoration: InputDecoration(
                     labelText: 'E-mail',
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe o e-mail';
+                    }
+                    return null;
+                  },
                 ),
-                SizedBox(height: 16.0),
+                SizedBox(height: 8.0),
                 TextFormField(
                   controller: senhaController,
                   decoration: InputDecoration(
                     labelText: 'Senha',
                   ),
-                  validator: (value) {
-                    if (value!.length > 4) {
-                      return 'senha fraca';
-                    }
-                  },
                   obscureText: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe a senha';
+                    } else if (value.length < 8) {
+                      return 'A senha deve ter pelo menos 8 caracteres';
+                    }
+                    return null;
+                  },
                 ),
-                SizedBox(height: 24.0),
+                SizedBox(height: 16.0),
                 ElevatedButton(
                   onPressed: () {
-                    var aviso = SnackBar(
-                        content: const Text("Senha muito curta"),
-                        action: SnackBarAction(
-                            label: 'Desfazer', onPressed: () {}));
-
-                    if (tamanhosenha < 8) {
-                      ScaffoldMessenger.of(context).showSnackBar(aviso);
+                    if (_formKey.currentState!.validate()) {
+                      // Ação para cadastrar o usuário
                     }
                   },
                   child: Text('Cadastrar'),
@@ -76,6 +91,8 @@ class _Cadastro extends State<Cadastro> {
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
