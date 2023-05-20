42ba111f954f79269a04ec576243f26ee2789fd0
kalexgama <kassio.alex.g@gmail.com>
2023-05-19 20:58:30 -0400

    list view aparecendo no cadastro porem sem adicionar logins

--- lib/src/cadastro.dart
+++ lib/src/cadastro.dart
@@ -30,67 +30,88 @@ class _CadastroState extends State<Cadastro> {
             ),
           ),
         ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: nomeController,
+                          decoration: InputDecoration(
+                            labelText: 'Nome',
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informe o nome';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 8.0),
+                        TextFormField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            labelText: 'E-mail',
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informe o e-mail';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 8.0),
+                        TextFormField(
+                          controller: senhaController,
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informe a senha';
+                            } else if (value.length < 8) {
+                              return 'A senha deve ter pelo menos 8 caracteres';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              // Ação para cadastrar o usuário
+                            }
+                          },
+                          child: Text('Cadastrar'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ListView(
+              shrinkWrap: true,
               children: [
-                TextFormField(
-                  controller: nomeController,
-                  decoration: InputDecoration(
-                    labelText: 'Nome',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Informe o nome';
-                    }
-                    return null;
-                  },
+                // Conteúdo da ListView aqui
+                ListTile(
+                  title: Text('Item 1'),
                 ),
-                SizedBox(height: 8.0),
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: 'E-mail',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Informe o e-mail';
-                    }
-                    return null;
-                  },
+                ListTile(
+                  title: Text('Item 2'),
                 ),
-                SizedBox(height: 8.0),
-                TextFormField(
-                  controller: senhaController,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Informe a senha';
-                    } else if (value.length < 8) {
-                      return 'A senha deve ter pelo menos 8 caracteres';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      // Ação para cadastrar o usuário
-                    }
-                  },
-                  child: Text('Cadastrar'),
-                ),
+                // Adicione mais itens conforme necessário
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
